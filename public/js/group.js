2f43ffc22e8f06139ef2618ccc1f1b5c0fe784b6
Thomas Szentirmay <tlszent@outlook.com>
2023-07-10 14:06:14 -0400

    group javascript logic done

--- public/js/group.js
+++ public/js/group.js
@@ -1,1 +1,21 @@
 const groupForm = document.querySelector('#group-form');
+
+function handleGroupSubmit(event) {
+    event.preventDefault();
+
+    const nameInput = document.querySelector('#group-name-input');
+
+    fetch('/group', {
+        method: 'POST',
+        headers: {
+            'CONTENT-Type': 'application/json'
+        },
+        body: JSON.stringify({
+            name: nameInput.value
+        })
+    }).then(res => {
+        alert('Group successfully added!');
+    });
+};
+
+groupForm.addEventListener(submit, handleGroupSubmit);

a8bf041e0b31756b4ea6cd14b08c3ea93b36c1b5
Thomas Szentirmay <tlszent@outlook.com>
2023-07-12 11:46:22 -0400

    handlebars practice

--- controllers/view_routes.js
+++ controllers/view_routes.js
@@ -2,7 +2,9 @@ const router = require('express').Router();
 const path = require('path');
 
 router.get('/', (req, res) => {
-    res.render('index');
+    res.render('index', {
+        username: 'JD'
+    });
 })
 
 router.get('/about', (req, res) => {
